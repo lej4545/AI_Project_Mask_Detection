d6f918be103cff3bd27ef90c41292a234ca384df
lej4545 <lej4545@naver.com>
2021-08-30 06:57:34 +0900

    Densenet 모델

--- 02_Projects/Densenet_model.py
+++ 02_Projects/Densenet_model.py
@@ -4,16 +4,18 @@ keras.__version__ # 버전 확인
 from google.colab import drive # 구글 드라이브에서 구글 colab 연동
 drive.mount('/content/drive') # 구글 드라이브 마운트
 
-import os # os모듈 임포트
+import os
 for dirname, _, filenames in os.walk('/content/drive/My Drive/PART1_ai_project_20210824_0830/02_Projects/03_LeeEunJin'):
-    for filename in filenames: # 아마도 위에 폴더 안에 있는 파일들을 불러와서
-        os.path.join(dirname, filename) # 폴더 이름과 파일 이름을 합쳐주는 것 같다.
+    for filename in filenames:
+        os.path.join(dirname, filename)
 
-import pandas as pd # 필요한 모듈 임포트
+import pandas as pd
 import numpy as np
 import seaborn as sns
 from pandas import DataFrame
 
+
+## 데이터 들어 있는 폴더
 traindir='/content/drive/My Drive/PART1_ai_project_20210824_0830/01_Images/Train' # 학습할 데이터가 들어있는 폴더 경로
 validdir='/content/drive/My Drive/PART1_ai_project_20210824_0830/01_Images/Validation' #  검증하기 위한 데이터가 들어있는 폴더 경로
 testdir='/content/drive/My Drive/PART1_ai_project_20210824_0830/01_Images/Test' # 테스트 데이터가 들어있는 폴더 경로
@@ -26,13 +28,13 @@ import matplotlib.image as mpimg
 
 plt.figure(figsize=(20,20)) # 잘 불러오는지 확인하기 위해 임의의 5장 사진 출력
 for i in range(5):
-    file=random.choice(os.listdir(path)) # 디렉토리에 있는 파일 리스트를 가져온다.
+    file=random.choice(os.listdir(path))
     img_path=os.path.join(path,file)
     image=mpimg.imread(img_path)
     ax=plt.subplot(1,5,i+1)
     plt.imshow(image)
 
-from tensorflow.keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img #imagedatagenerator 임포트
+from tensorflow.keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
 
 # train_data, test_data 전처리
 train_data=ImageDataGenerator(rescale=1.0/255,
@@ -48,26 +50,29 @@ train_data=ImageDataGenerator(rescale=1.0/255,
 train_generator = train_data.flow_from_directory(directory=traindir,target_size=(256,256),class_mode='binary', batch_size=32)
 test_data=ImageDataGenerator(rescale=1.0/255)
 # valid generator 도 train generator와 마찬가지로 진행
-valid_generator = test_data.flow_from_directory(directory=validdir,target_size=(256,256),class_mode='binary',batch_size=32)
+valid_generator  test_data.flow_from_directory(directory=validdir,target_size=(256,256),class_mode='binary',batch_size=32)
 # test 데이터도 train generator 에서 진행한 방식과 동일하고 추가적으로 rescaling을 진행
 test_generator = test_data.flow_from_directory(directory=testdir,target_size=(256,256),class_mode='binary',batch_size=32,shuffle=False)
 
 print(valid_generator.class_indices)
 
+
+# Densenet 모델 적용
+
 from tensorflow.keras import layers, Sequential
-from keras.applications.vgg19 import VGG19 # VGG19 모델 적용
+from keras.applications.densenet import DenseNet201 # VDenseNet201모델 적용
 from keras.applications.vgg19 import preprocess_input
 import tensorflow as tf
 
 # 사전 교육된 기본 모델 생성
-vgg19 = VGG19(weights='imagenet',include_top=False,input_shape=(256,256,3))
+densenet201 = DenseNet201(weights='imagenet',include_top=False,input_shape=(256,256,3))
 
-for layer in vgg19.layers:
+for layer in densenet201.layers:
     layer.trainable = False
 
 
 vmodel = Sequential() # 모형 작성
-vmodel.add(vgg19) # VGG19 기반 모델 추가
+vmodel.add(densenet201) # VGG19 기반 모델 추가
 vmodel.add(layers.Flatten()) # 평평하게 만들어 Denser 레이어로 전환을 하기 위함.(fully connected되는 부분)
 # Dense 첫번째 인자: 출력 뉴런의 수, 두번째 인자 activation 활성화 함수를 설정:
 # 'relu' : rectifier 함수로 은닉층에 주로 쓰임. 'sigmoid' : 이진 분류 문제에서 출력 층에 주로 쓰임, 'softmax' : 다중 클래스 분류 문제에서 출력 층에 주로 쓰임.
@@ -146,9 +151,12 @@ if vmodel.predict(image)[0][0]<0.5:
 else:
     print("No mask detected")
 
+
+# 학습된 데이터 TEST하기
+
 predictions=vmodel.predict(test_generator)
 predictions=np.round(predictions)
-y=np.hstack((np.zeros(483),np.ones(992-483)))
+y=np.hstack((np.zeros(50),np.ones(50)))
 
 m = tf.keras.metrics.BinaryAccuracy()
 m.update_state(y,predictions)
